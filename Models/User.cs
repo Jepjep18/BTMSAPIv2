30aef426e0d4bc26137fef70cfee9101f38bd83e
Jepjep18 <jeparnado@gmail.com>
2025-01-13 13:53:54 +0800

    Created User Model

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTMSAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public string Role { get; set; }
+
+        [RegularExpression(@"^\d{6}$", ErrorMessage = "Username must be in the format 000000.")]
+        public string Username { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string BusinessUnit { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public string? Esignature { get; set; }
+        public int IsActive { get; set; } = 1;
+        public DateTime? DateCreated { get; set; }
+
+    }
+}
